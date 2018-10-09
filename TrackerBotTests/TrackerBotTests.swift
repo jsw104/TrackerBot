cd41ed06af7c4c0d4e910d1d0c6c133214c3ff1f
Justin Wang <>
2018-10-09 12:38:52 -0600

    added failing test to test travis ci

--- TrackerBotTests/TrackerBotTests.swift
+++ TrackerBotTests/TrackerBotTests.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Justin. All rights reserved.
 //
 
+import Nimble
 import XCTest
 @testable import TrackerBot
 
@@ -24,6 +25,7 @@ class TrackerBotTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        expect(1 + 1).to(equal(3))
     }
     
     func testPerformanceExample() {
